d372593a3f458fed637a01e7e367d017f372ee8d
Marcel Sulek <marcel@sulek.eu>
2019-05-24 21:55:12 +0200

    RF, presun newsItem do vlastniho souboru

--- client/src/NewsColumn.svelte
+++ client/src/NewsColumn.svelte
@@ -1,32 +1,12 @@
 <script>
-import {getLayout, roundTime} from './Layouter.ts'
-import {entityRemover} from './entityRemover'
-import {toHumanTime} from './utils.ts'
+import NewsItem from './NewsItem.svelte'
 
 export let publisherId;
 export let data;
 export let displayPrint;
 
 const isPrintOnly = ['aktualne', 'irozhlas'].includes(publisherId);
-const entries = data.mainArticles.map(entry => {
-    const layout = getLayout(entry);
-    const style = `top: ${layout.top}px; height: ${layout.height}px`;
-    const timeDiff = layout.roundedEndDate.getTime() - layout.roundedStartDate.getTime();
-    const wasSeenOnlyOnce = timeDiff < 25 * 60000;
-    const lastSeen = entry.seenAt[entry.seenAt.length - 1];
-    const lastSeenRounded = roundTime(lastSeen);
-    const durationTitle = layout.wasUntilMidnight && lastSeenRounded.getHours() === 23 && lastSeenRounded.getMinutes() === 45
-      ? `, vydržel do dalšího dne`
-      : `, naposledy v ${toHumanTime(lastSeenRounded)}`;
-    const seenTitle = wasSeenOnlyOnce
-        ? `Zaznamenán jen jednou v ${toHumanTime(layout.roundedStartDate)}`
-        : `Zaznamenán poprvé v ${toHumanTime(layout.roundedStartDate)}` + durationTitle;
-    const title = `${entry.article.headline}\n\n${seenTitle}`;
-    const headline = entityRemover(entry.article.headline);
-    const perex = entityRemover(entry.article.perex);
-    const link = entry.article.link;
-    return {...entry, style, title, headline, perex, link};
-})
+
 </script>
 
 <div class="publisher-col publisher-col-{publisherId}">
@@ -49,13 +29,8 @@ const entries = data.mainArticles.map(entry => {
         </div>
     {/if}
     <div class="publisher-col-content">
-        {#each entries as {headline, perex, style, title, link}}
-        <div class="publisher-col-item publisher-col-item-background" {style} {title}>
-            <div class="publisher-col-item-content">
-                <a href="{link}" target="_blank" class="headline">{headline}</a>
-                <div class="perex">{perex}</div>
-            </div>
-        </div>
+        {#each data.mainArticles as entry}
+            <NewsItem {entry} />
         {/each}
     </div>
 </div>
---
+++ client/src/NewsItem.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {getLayout, roundTime} from './Layouter.ts'
+    import {entityRemover} from './entityRemover'
+    import {toHumanTime} from './utils.ts'
+    export let entry;
+    const layout = getLayout(entry);
+    const style = `top: ${layout.top}px; height: ${layout.height}px`;
+    const timeDiff = layout.roundedEndDate.getTime() - layout.roundedStartDate.getTime();
+    const wasSeenOnlyOnce = timeDiff < 25 * 60000;
+    const lastSeen = entry.seenAt[entry.seenAt.length - 1];
+    const lastSeenRounded = roundTime(lastSeen);
+    const durationTitle = layout.wasUntilMidnight && lastSeenRounded.getHours() === 23 && lastSeenRounded.getMinutes() === 45
+      ? `, vydržel do dalšího dne`
+      : `, naposledy v ${toHumanTime(lastSeenRounded)}`;
+    const seenTitle = wasSeenOnlyOnce
+        ? `Zaznamenán jen jednou v ${toHumanTime(layout.roundedStartDate)}`
+        : `Zaznamenán poprvé v ${toHumanTime(layout.roundedStartDate)}` + durationTitle;
+    const title = `${entry.article.headline}\n\n${seenTitle}`;
+    const headline = entityRemover(entry.article.headline);
+    const perex = entityRemover(entry.article.perex);
+    const link = entry.article.link;
+</script>
+<div class="publisher-col-item publisher-col-item-background" {style} {title}>
+    <div class="publisher-col-item-content">
+        <a href="{link}" target="_blank" class="headline">{headline}</a>
+        <div class="perex">{perex}</div>
+    </div>
+</div>
