de641ca3b04db71c919493d5de3e303653205791
Marcel Sulek <marcel@sulek.eu>
2019-05-26 21:31:24 +0200

    Fix Hashlink na lokalni casy misto UTC

--- client/src/hashLinks.ts
+++ client/src/hashLinks.ts
@@ -7,7 +7,10 @@ const getHashTimePart = function (date: Date) {
 };
 
 const getHashDatePart = function (date: Date) {
-    return date.toISOString().substr(0, 10);
+    const year = date.getFullYear().toString();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = (date.getDate()).toString().padStart(2, '0');
+    return `${year}-${month}-${day}`; // not ISO because we want local time
 };
 
 const getColumnPart = () => {
