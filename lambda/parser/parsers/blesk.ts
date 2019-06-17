51dd8c890081a70b29ea6996d51bc1b3faa154ae
Marcel Sulek <marcel@sulek.eu>
2019-06-17 22:46:07 +0200

    Fix Blesk parseru aby bral i otvirak

--- lambda/parser/parsers/blesk.ts
+++ lambda/parser/parsers/blesk.ts
@@ -6,7 +6,7 @@ const bleskParser: IParser = async (file) => {
     const content = await downloadObject(file);
     const root = parse(content.toString());
     const elements = [...(root as any).querySelectorAll('.item-middle-top'), ...(root as any).querySelectorAll('.item-middle')];
-    return elements
+    const pageArticles = elements
         .map((e: any): IArticleData | null => {
             const headlineElement = e.querySelector('h3');
             if (!headlineElement) {
@@ -15,15 +15,26 @@ const bleskParser: IParser = async (file) => {
             const headline = headlineElement.rawText.trim();
             const perex = '';
 
-             const linkElement = e.querySelector('a');
+            const linkElement = e.querySelector('a');
             if (!linkElement || !linkElement.attributes) {
                 return null;
             }
             const link = linkElement.attributes.href;
-            return { headline, perex, link };
+            return {headline, perex, link};
         })
-        .filter((i: IArticleData | null) => i !== null)
-        .slice(0, 10);
+        .filter((i: IArticleData | null) => i !== null) as IArticleData[];
+    const promoImage = (root as any).querySelector('img.promoImage');
+    if(promoImage) {
+        const articleData: IArticleData = {
+            headline: promoImage.attributes['alt'],
+            perex: '',
+            link: promoImage.parentNode.attributes.href
+        };
+        if(articleData.headline && articleData.link) {
+            pageArticles.unshift(articleData);
+        }
+    }
+    return pageArticles.slice(0, 10);
 };
 
 export default bleskParser;
